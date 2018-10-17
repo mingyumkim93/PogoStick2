16c9609f2a1494c342e32940847ca8fdc3ffc097
MingyumKim <Mingyum93@gmail.com>
2018-10-17 16:05:25 +0300

    creating 2 simple scenes

---
+++ Assets/cubeObstacle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeObstacle : MonoBehaviour {
+
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
+    
+    float movementFactor; //0 for not moved, 1 for fully moved.
+
+    Vector3 startingPos;
+    
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+    
+    void Update()
+    {
+        //todo protect against period is zero
+        if (period <= Mathf.Epsilon) return;
+        float cycles = Time.time / period; //grows continually from 0
+        const float tau = Mathf.PI * 2; //about 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = rawSinWave / 2f + 0.5f;
+        Vector3 offset = movementFactor * movementVector;
+        transform.position = startingPos + offset;
+    }
+}
