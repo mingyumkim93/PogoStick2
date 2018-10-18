1e2de4fb0249766f2551ba51f7f526cc59e45a1b
MingyumKim <Mingyum93@gmail.com>
2018-10-19 01:39:29 +0300

    making re-load scene sequence. downloading ProGrids.

---
+++ Assets/PogoStickUpperPart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class PogoStickUpperPart : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Terrain")
+            StartCoroutine(ReloadScene());
+    }
+
+    IEnumerator ReloadScene()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(1);
+    }
+}
